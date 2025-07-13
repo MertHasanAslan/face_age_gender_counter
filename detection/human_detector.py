a9465813adbb3e0711713fee43a48a6c741e0a75
merthasanaslan <merthasanaslan325@gmail.com>
2025-07-13 20:01:54 +0300

    I debugged but it still count same person twice so that I need to upgrade my tracking system however since I want to run this in less powerful devices (I will turn it to tfLite later) I cannot use better tracking systems.

--- detection/human_detector.py
+++ detection/human_detector.py
@@ -13,7 +13,7 @@ model = YOLO("yolov8n.pt")
 
 # euclidean means that closest path from one object to another
 # distance threshold means that if a object is closer than x tracker will approach it they are like the same objects
-tracker = Tracker(distance_function= "euclidean", distance_threshold=10) 
+tracker = Tracker(distance_function= "euclidean", distance_threshold=40) 
 
 cap = cv2.VideoCapture(video_path)
 
@@ -40,25 +40,28 @@ while True:
         cls_id = int(box.cls[0]) #take the id of the object
         if model.names[cls_id] == "person": # model.names is a dict for ex. {id1: "person", id2: "biycycle", id3: "car"..}
 
-            conf=float(box.conf[0]) #this is the probability of that object
+            conf = float(box.conf[0]) #this is the probability of that object
             if conf > 0.55: #this is a threshold value if prob > 0.50 show it!
                 x1, y1, x2, y2 = map(int, box.xyxy[0]) #coordinates of the box take it as integer
                 label = f"{model.names[cls_id]}, {conf:.2f}"
 
                 cx, cy = int((x1 + x2)/2), int((y1 + y2)/2) # center of the box
-                detections.append(Detection(np.array([[cx, cy]]))) #append to the detections as the Norfair format
+                detections.append(Detection(points= np.array([[cx, cy]]))) #append to the detections as the Norfair format
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 117, 44), 2)
+                cv2.putText(frame, label, (x1, (y1-10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 117, 44), 2)
 
     tracked_objects = tracker.update(detections)
 
     for object in tracked_objects:
+        if object.live_points == 0:
+            continue
+        
         x, y = object.estimate[0]
         track_id = object.id
 
         unique_ids.add(track_id)
 
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 117, 44), 2)
-        cv2.putText(frame, label, (x1, (y1-10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 117, 44), 2)
-        cv2.putText(frame, f"ID: {track_id}", (x1, y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 117, 44), 2)
+        cv2.putText(frame, f"ID: {track_id}", (int(x), int(y) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 117, 44), 2)
 
 
     (text_width, text_height), _ = cv2.getTextSize(f"Human Count: {len(results.boxes)}", cv2.FONT_HERSHEY_COMPLEX, 1, 2)
