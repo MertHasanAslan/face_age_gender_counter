b7e1e4a89439e59979abd3a16f1981b09858124c
merthasanaslan <merthasanaslan325@gmail.com>
2025-07-13 17:16:33 +0300

    Trying to track people so that it will not count the same people twice but they are some bugs.

--- detection/human_detector.py
+++ detection/human_detector.py
@@ -1,5 +1,8 @@
 import cv2
 from ultralytics import YOLO
+from norfair import Detection, Tracker
+import numpy as np
+
 
 video_path = "data/people_walking.mp4" #video path
 
@@ -8,11 +11,17 @@ video_path = "data/people_walking.mp4" #video path
  #I used nano model but you can use small, medium,...
 model = YOLO("yolov8n.pt")
 
+# euclidean means that closest path from one object to another
+# distance threshold means that if a object is closer than x tracker will approach it they are like the same objects
+tracker = Tracker(distance_function= "euclidean", distance_threshold=10) 
+
 cap = cv2.VideoCapture(video_path)
 
 if not cap.isOpened():
     print("Video could not open!")
     exit()
+
+unique_ids = set() # will save ids here since set do not save same id more than one
 
 while True:
     ret, frame =cap.read() #ret is a boolean that indicates if video opened or not
@@ -25,6 +34,8 @@ while True:
     # we need to return first element of the list
     results = model(frame)[0] 
 
+    detections = []
+
     for box in results.boxes:
         cls_id = int(box.cls[0]) #take the id of the object
         if model.names[cls_id] == "person": # model.names is a dict for ex. {id1: "person", id2: "biycycle", id3: "car"..}
@@ -33,14 +44,27 @@ while True:
             if conf > 0.55: #this is a threshold value if prob > 0.50 show it!
                 x1, y1, x2, y2 = map(int, box.xyxy[0]) #coordinates of the box take it as integer
                 label = f"{model.names[cls_id]}, {conf:.2f}"
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 117, 44), 2)
-                cv2.putText(frame, label, (x1, (y1-10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 117, 44), 2)
+
+                cx, cy = int((x1 + x2)/2), int((y1 + y2)/2) # center of the box
+                detections.append(Detection(np.array([[cx, cy]]))) #append to the detections as the Norfair format
+
+    tracked_objects = tracker.update(detections)
+
+    for object in tracked_objects:
+        x, y = object.estimate[0]
+        track_id = object.id
+
+        unique_ids.add(track_id)
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 117, 44), 2)
+        cv2.putText(frame, label, (x1, (y1-10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 117, 44), 2)
+        cv2.putText(frame, f"ID: {track_id}", (x1, y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 117, 44), 2)
 
 
     (text_width, text_height), _ = cv2.getTextSize(f"Human Count: {len(results.boxes)}", cv2.FONT_HERSHEY_COMPLEX, 1, 2)
     x = frame.shape[1] - text_width - 10
     y = text_height + 10
-    cv2.putText(frame, f"Human Count: {len(results.boxes)}",(x, y) , cv2.FONT_HERSHEY_COMPLEX, 1, (255, 117, 44) , 2)
+    cv2.putText(frame, f"Human Count: {len(unique_ids)}",(x, y) , cv2.FONT_HERSHEY_COMPLEX, 1, (255, 117, 44) , 2)
 
     cv2.imshow("video", frame)
 
